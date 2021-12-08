2d0a7d4aa9caebaa3120b37e8cf9d155993f42e1
bjornaer <max.schulkin@deliveryhero.com>
2021-12-08 20:59:15 +0100

    update setup file

--- setup.py
+++ setup.py
@@ -7,19 +7,14 @@ setup(
     version=__version__,
     description="perform bayesian inference over physical models",
     url="https://github.com/bjornaer/havi",
-    download_url="https://github.com/bjornaer/havi/archive/refs/tags/v.0.0.1-alpha.tar.gz",
+    download_url="https://github.com/bjornaer/havi/archive/refs/tags/v.0.0.2-alpha.tar.gz",
     author="Francisco Grings, Maximiliano Schulkin",
     author_email="max.schulkin@gmail.com",
     keywords = ['physics', 'scatering', 'bayesian', 'inference', 'data', 'science'],
     packages=find_packages(include=["havi"]),
     license="Apache2.0",
     install_requires=["numpy", "torch", "pyro-ppl", "pyro-api"],
-    entry_points={
-        "console_scripts": [
-            "titan = titan.titan:cli",
-        ]
-    },
-      classifiers=[  # Optional
+    classifiers=[  # Optional
     # How mature is this project? Common values are
     #   3 - Alpha
     #   4 - Beta
