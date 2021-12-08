fd9e15c058ac7a5cdb0d3b7509ecfe73a6fba0b9
bjornaer <max.schulkin@deliveryhero.com>
2021-12-08 20:49:19 +0100

    initial draft of lib

---
+++ havi/__init__.py
@@ -0,0 +1,1 @@
+from havi.havi import Havi, Tensor
---
+++ setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+
+from havi import __version__
+
+setup(
+    name="havi",
+    version=__version__,
+    description="perform bayesian inference over physical models",
+    url="https://github.com/bjornaer/heimdallr",
+    author="Francisco Grings, Maximiliano Schulkin",
+    author_email="max.schulkin@gmail.com",
+    keywords = ['physics', 'scatering', 'bayesian', 'inference', 'data', 'science'],
+    packages=find_packages(include=["havi"]),
+    license="Apache2.0",
+    install_requires=["numpy", "torch", "pyro-ppl", "pyro-api"],
+    entry_points={
+        "console_scripts": [
+            "titan = titan.titan:cli",
+        ]
+    },
+      classifiers=[  # Optional
+    # How mature is this project? Common values are
+    #   3 - Alpha
+    #   4 - Beta
+    #   5 - Production/Stable
+    'Development Status :: 3 - Alpha',
+
+    # Indicate who your project is intended for
+    'Intended Audience :: Scientific Community',
+    'Topic :: Data Analysis :: Bayesian Inference',
+
+    # Pick your license as you wish
+    'License :: OSI Approved :: Apache 2 License',
+
+    # Specify the Python versions you support here. In particular, ensure
+    # that you indicate whether you support Python 2, Python 3 or both.
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.9',
+  ],
+)
